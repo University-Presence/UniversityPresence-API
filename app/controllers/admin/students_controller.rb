8d85e5e5e0ad95198eac78aeaba866ead20e9898
YiipHuhu <alan.gabriel@nullbug.dev>
2025-03-31 12:35:27 -0300

    ajustando students_controller que estava gernado erro na função index

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -5,6 +5,7 @@ module Admin
     before_action :authenticate_user_with_jwt
 
     def index
+      students = Student.all
       students = students.where(class_room_id: params[:class_room_id]) if params[:class_room_id].present?
       students = students.ransack(params[:q])
       @students = students.result.page(params[:page]).per(params[:per_page] || 10)
