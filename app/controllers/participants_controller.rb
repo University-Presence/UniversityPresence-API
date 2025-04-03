9655ac682b56e6dd540a7656b365d00c4474c30c
CarHSMig <carlos.henrique@nullbug.dev>
2025-04-03 00:58:30 -0300

    validação da localização do participante ao confirmar a presença

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -6,11 +6,10 @@ class ParticipantsController < ApplicationController
 
   def confirm_presence
     student = find_student
-    event = event_id
+    event = Event.find_by(id: params["event_id"])
     participant = find_participant(student, event)
-    location = confirm_presence_params["location"]
     
-    update_participant_presence(participant, location)
+    update_participant_presence(participant, event)
   end
 
   def show_event
@@ -27,12 +26,12 @@ class ParticipantsController < ApplicationController
 
   def confirm_presence_params
     params.require(:data).require(:attributes).permit(
-      :name, :ra, :location
+      :ra, :longitude, :latitude
     )
   end
 
   def find_student
-    student = Student.find_by(name: confirm_presence_params["name"], ra: confirm_presence_params["ra"])
+    student = Student.find_by( ra: confirm_presence_params["ra"] )
     
     unless student
       render json: { error: 'Nome ou RA inválidos' }, status: :not_found
@@ -42,12 +41,8 @@ class ParticipantsController < ApplicationController
     student
   end
 
-  def event_id
-    params["event_id"]
-  end
-
-  def find_participant(student, event_id)
-    participant = Participant.find_by(student_id: student.id, event_id: )
+  def find_participant(student, event)
+    participant = Participant.find_by(student_id: student.id, event: event)
     
     unless participant
       render json: { error: 'Participante não encontrado' }, status: :not_found
@@ -57,9 +52,27 @@ class ParticipantsController < ApplicationController
     participant
   end
 
-  def update_participant_presence(participant, location)
-    participant.update(present: true, location: location)
+  def update_participant_presence(participant, event)
+    event_latitude = format('%.10f', event.latitude).to_f
+    event_longitude = format('%.10f', event.longitude).to_f
 
+    location_service = Location::GetLocationParticipant.new(
+      confirm_presence_params[:latitude],
+      confirm_presence_params[:longitude],
+      event_latitude,
+      event_longitude
+    )
+
+    distance = location_service.distance(confirm_presence_params[:latitude], confirm_presence_params[:longitude], event.latitude, event.longitude)
+
+    if distance > Location::GetLocationParticipant::ALLOWED_RADIUS
+      render json: { error: 'Participante está fora da área do evento' }, status: :unprocessable_entity
+      return
+    end
+
+    location = location_service.perform
+
+    participant.update(present: true, location: location["display_name"])
     render jsonapi: participant, include: include_options, class: { Participant: SerializableParticipant, Event: SerializableEvent }, status: :ok
   end
 end
