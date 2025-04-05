dc9c0ae93b5ba6ccaa1cd2d31ad0ebc1801fc937
CarHSMig <carlos.henrique@nullbug.dev>
2025-04-05 16:50:27 -0300

    atualização do seed para criação do evento

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,10 @@ Course.create(name:"Enfermagem", periods: 10)
 
 class_room = ClassRoom.create!(name: "Turma A", course: course)
 
+Event.create!([
+  { name: "Aula de Algoritmos", description: "Aula de Algoritmos", event_start: Time.current, event_end: Time.current + 1.hour, course: course, class_rooms: [class_room], location: "unipar", latitude: -27.595377, longitude: -48.548038 },
+])
+
 Student.create!([
   { name: "Carlos H. Migon", ra: "241403-1", class_room: class_room },
   { name: "Alan G. G. Przyvara", ra: "249693-1", class_room: class_room }
