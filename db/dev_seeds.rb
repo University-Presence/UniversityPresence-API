be598321f54a543276f3eea3361a2c9e759e1a99
CarHSMig <carlos.henrique@nullbug.dev>
2025-04-06 19:20:28 -0300

    atualização dos seed para deploy

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -19,6 +19,32 @@ Course.create(name:"Enfermagem", periods: 10)
 
 class_room = ClassRoom.create!(name: "Turma A", course: course)
 
+Event.create!([
+  {
+    name: "Aula de Algoritmos",
+    description: "Aula de Algoritmos",
+    event_start: Time.current,
+    event_end: Time.current + 1.hour,
+    course: course,
+    class_rooms: [class_room],
+    location: {
+      amenity: "Universidade Paranaense UNIPAR",
+      road: "Rua São Mateus",
+      town: "Francisco Beltrão",
+      municipality: "Região Geográfica Imediata de Francisco Beltrão",
+      state_district: "Região Geográfica Intermediária de Cascavel",
+      state: "Paraná",
+      "ISO3166-2-lvl4": "BR-PR",
+      region: "Região Sul",
+      postcode: "85605-000",
+      country: "Brasil",
+      country_code: "br"
+    },
+    latitude: -26.08671845,
+    longitude: -48.548038
+  }
+])
+
 Student.create!([
   { name: "Carlos H. Migon", ra: "241403-1", class_room: class_room },
   { name: "Alan G. G. Przyvara", ra: "249693-1", class_room: class_room }
